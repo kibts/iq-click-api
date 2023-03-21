2f0ffc4fe918b6cb58e4a047e32c1075193b8847
kibts <124083041+kibts@users.noreply.github.com>
2023-03-21 11:58:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,11 @@ require_once 'kclick_client.php';
 $client = new KClickClient('https://waeeaw.comptinser.pw/api.php?sub_id_1={sub_id_1}&sub_id_2={sub_id_2}&sub_id_3={sub_id_3}&param1={param1}', 'R5f5HZ');
 $client->sendAllParams();       // to send all params from page query
 $client->params('sub_id_1={sub_id_1}&sub_id_2={sub_id_2}&sub_id_3={sub_id_3}&param1={param1}');  // send params like param1=c&param2=b
-// $client->sendUtmLabels();     // to send only utm labels
+$client->sendUtmLabels('sub_id_1={sub_id_1}&sub_id_2={sub_id_2}&sub_id_3={sub_id_3}&param1={param1}');     // to send only utm labels
 // $client->debug();              // to enable debug mode and show the errors
 // $client->currentPageAsReferrer(); // to send current page as referrer value
 // $client->keyword('KEYWORD');  // send custom keyword
 // $client->param('extra_param_5', '123'); // you can send any params
-// $client->execute();             // request to api, show the output and continue
-$subid=$client-&gt;getSubId();
-$client->executeAndBreak();     // to stop page execution if there is redirect or some output
+$client->execute();             // request to api, show the output and continue
+// $client->executeAndBreak();     // to stop page execution if there is redirect or some output
 ?>
